7516b4ba24f262a3845c8690008ad68de4317b06
Ali Almohaya <AliAlmohaya@gmail.com>
2018-05-09 20:43:10 +0300

    add webpack nad babel configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,19 @@
 const path = require('path');
+
+module.exports = {
+    entry: {
+        app: path.join(__dirname, './src/index.js')
+    },
+    output: {
+        path: path.join(__dirname, './public'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [{
+                test: /\.js$/,
+                loader: 'babel-loader'
+            }
+
+        ]
+    }
+}
