0319b7b12d7f31763b5cf1f7027cda73912d292d
Ali Almohaya <AliAlmohaya@gmail.com>
2018-05-09 23:28:15 +0300

    add some component

---
+++ src/components/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const App = () => (
+  <h1>This is App component</h1>
+);
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import propTypes from 'prop-types';
 
-const App = (props) => (
-  <h1>
-    {props.header}
-  </h1>
-);
-
-App.propTypes = {
-  header: propTypes.string
-};
+import App from './components/App.js';
 
 ReactDOM.render(
-  <App header={'Hello'} />,
+  <App />,
   document.getElementById('root')
 );
