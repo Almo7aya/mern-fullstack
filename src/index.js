6d1fde5d55327d47af2296e3d62fcc13ce325887
Ali Almohaya <AliAlmohaya@gmail.com>
2018-05-09 23:15:48 +0300

    add prop types to the project

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import propTypes from 'prop-types';
+
+const App = (props) => (
+  <h1>
+    {props.header}
+  </h1>
+);
+
+App.propTypes = {
+  header: propTypes.string
+};
 
 ReactDOM.render(
-  React.createElement('p', null, 'hello form react'),
+  <App header={'Hello'} />,
   document.getElementById('root')
 );
