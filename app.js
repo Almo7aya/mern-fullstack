db7908b3b33f406a20a0d748954747a482afc301
Ali Almohaya <AliAlmohaya@gmail.com>
2018-05-10 18:12:31 +0300

    add node-sass-middleware

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
-import express from  'express';
+import express from 'express';
+import path from 'path';
+import nodeSass from 'node-sass-middleware';
+
 const app = express();
+
 
 // import routes
 import indexRoutes from './routes/index';
@@ -7,6 +11,11 @@ import indexRoutes from './routes/index';
 
 // template engine
 app.set('view engine', 'ejs');
+
+app.use(nodeSass({
+  src: path.join(__dirname, 'sass'),
+  dest: path.join(__dirname, 'public')
+}));
 
 // middleware
 app.use(express.static('public'));
