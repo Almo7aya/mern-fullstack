c31e2c80b60e1331f1badbb8140c9a9bb0886b8a
Ali Almohaya <AliAlmohaya@gmail.com>
2018-05-10 17:54:52 +0300

    fix `package.js` component

--- src/components/Package.js
+++ src/components/Package.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
 const Package = ({name, version}) => (
-  <li>
-    <p> Name => {name} / Version => {version}</p>
-  </li>
+  <tr>
+    <td>{name}</td>
+    <td>{version}</td>
+  </tr>
 );
 
 
